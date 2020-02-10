e2249eef5c2fdd9e314c663aff95b6a7abd182df
Gustavo Novaes <gustavonovaes93@gmail.com>
2020-02-10 15:50:53 -0300

    feat: Shows author gravatar

--- resources/views/discussions/index.blade.php
+++ resources/views/discussions/index.blade.php
@@ -4,10 +4,25 @@
 
 @foreach($discussions as $discussion)
 <div class="card mb-3">
-    <div class="card-header">{{ $discussion->title }}</div>
+    <div class="card-header">
+        <div class="d-flex justify-content-between align-content-center">
+            <div>
+                <img style="width: 49px; height: 49px;"
+                     class="rounded-circle"
+                     src="{{ Gravatar::src($discussion->author->email) }}"
+                     alt="" />
+                <strong class="ml-2 text-uppercase">{{ $discussion->author->name }}</strong>
+            </div>
+
+            <div>
+                <a href="{{ route('discussions.show', $discussion->slug) }}"
+                   class="btn btn-success btn-sm">View</a>
+            </div>
+        </div>
+    </div>
 
     <div class="card-body">
-        {!! $discussion->content !!}
+        {!! $discussion->title !!}
     </div>
 </div>
 @endforeach
